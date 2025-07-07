8c9b877679aee0afe13b5eb5af4febbd095edc23
lauro-vitor-pina <lauro.pina@outlook.com>
2025-07-07 17:47:37 -0300

    Fix loading environment vars

--- src/config/env_loader.php
+++ src/config/env_loader.php
@@ -4,7 +4,7 @@
 
 $host = $_SERVER['SERVER_NAME'];
 
-if ($host == 'localhost' || $host = '127.0.01') {
+if ($host == 'localhost' || $host == '127.0.01') {
     require_once(__DIR__ . '/env_vars.local.php');
 } else {
     require_once(__DIR__ . '/env_vars.production.php');
